0e422776b1e53e888edefe39f50857b2f143cb02
tinfeek80 <jinl33@nycstudents.net>
2026-01-29 18:57:24 -0500

    Reset fields when submit. Delete Data Output

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var key = "Name";
+var key = ""; // What is the purpose of this???? :sad:
 
 let Save_Key = document.querySelector("#saveKey")
 let Save_Value = document.querySelector("#saveValue")
@@ -10,6 +10,7 @@ let Update_Key = document.querySelector("#updateKey")
 let Update_Value = document.querySelector("#updateValue")
 
 let Delete_Key = document.querySelector("#deleteKey")
+let Delete_Output = document.querySelector("#deleteOutput")
 
 // SAVE
 document.querySelector(".save").onclick = function() {
@@ -17,6 +18,8 @@ document.querySelector(".save").onclick = function() {
     console.log(Save_Key.value);
     console.log(Save_Value.value);
     localStorage.setItem(Save_Key.value, Save_Value.value);
+    Save_Key.value = "";
+    Save_Value = "";
 };
 
 
@@ -25,16 +28,23 @@ document.querySelector(".load").onclick = function() {
     console.log("Load");
     console.log(localStorage.getItem(Load_Key.value));
     Load_Value.value = localStorage.getItem(Load_Key.value);
+    Load_Key.value = "";
 };
 
 // Update
 document.querySelector(".update").onclick = function() {
     console.log("Update");
     localStorage.setItem(Update_Key.value, Update_Value.value);
+    Update_Key.value = "";
+    Update_Value.value = "";
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
     console.log("Delete");
     localStorage.removeItem(Delete_Key.value);
+
+    if (localStorage.getItem(Delete_Key.value) == null){
+        Delete_Output.value = `${Delete_Key.value} has been deleted.`;
+    }
 };
